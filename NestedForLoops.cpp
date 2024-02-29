821f69962d6e65081131568303bead37d2b47fc9
Sergio Chiquete Castro <sergiogccastro@gmail.com>
2024-02-28 18:53:37 -0800

    Update NestedForLoops.cpp

--- NestedForLoops.cpp
+++ NestedForLoops.cpp
@@ -26,3 +26,35 @@ int main() {
 
 
 }
+
+
+/*
+
+
+In the first row (i = 0):
+
+For j = 0, i + j = 0, so it prints a space.
+For j = 1, i + j = 1, so it prints a space.
+For j = 2, i + j = 2, so it prints a space.
+For j = 3, i + j = 3, so it prints an asterisk *.
+In the second row (i = 1):
+
+For j = 0, i + j = 1, so it prints a space.
+For j = 1, i + j = 2, so it prints a space.
+For j = 2, i + j = 3, so it prints an asterisk *.
+For j = 3, i + j = 4, so it prints an asterisk *.
+In the third row (i = 2):
+
+For j = 0, i + j = 2, so it prints a space.
+For j = 1, i + j = 3, so it prints an asterisk *.
+For j = 2, i + j = 4, so it prints an asterisk *.
+For j = 3, i + j = 5, so it prints an asterisk *.
+In the fourth row (i = 3):
+
+For j = 0, i + j = 3, so it prints an asterisk *.
+For j = 1, i + j = 4, so it prints an asterisk *.
+For j = 2, i + j = 5, so it prints an asterisk *.
+For j = 3, i + j = 6, so it prints an asterisk *.
+
+
+*/
