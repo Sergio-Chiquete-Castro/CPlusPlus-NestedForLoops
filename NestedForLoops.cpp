0151802b59804185dc187a25f9b45853517cff9c
Sergio Chiquete Castro <sergiogccastro@gmail.com>
2024-02-28 18:50:30 -0800

    Create NestedForLoops.cpp

---
+++ NestedForLoops.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+
+
+	for (int i = 0; i < 4; i++) { //rows
+
+		for (int j = 0; j < 4; j++) {//columns
+
+			if (i + j > 4 - 1) {
+				cout << "*";
+			}
+			else {
+				cout << "*";
+			}
+
+		}
+
+		cout << endl;
+	}
+
+ 
+
+
+}
